695349369ca99ffa5964dfe6aa06479697060a3d
CM\moa.bergstrom <moa.bergstrom@futuregames.nu>
2022-03-28 12:01:01 +0200

    traffic light changes color every 5 seconds

---
+++ Assets/Scripts/YellowToRedLight.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YellowToRedLight : MonoBehaviour
+{
+    public GameObject RedLightPrefab;
+    Vector2 Position = new Vector2(7, 1);
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(TurnRed());
+    }
+    private IEnumerator TurnRed()
+    {
+        yield return new WaitForSeconds(5);
+        Instantiate(RedLightPrefab, Position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+}
