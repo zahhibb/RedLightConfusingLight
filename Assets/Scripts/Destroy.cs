2234af1a527cba4c87ce3df81dfcc92c231ec714
CM\moa.bergstrom <moa.bergstrom@futuregames.nu>
2022-03-28 17:33:22 +0200

    Level mostly done

--- Assets/Scripts/Destroy.cs
+++ Assets/Scripts/Destroy.cs
@@ -6,7 +6,15 @@ public class Destroy : MonoBehaviour
 {
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Debug.Log(collision);
-        Destroy(gameObject);
+        GetComponent<SpriteRenderer>().enabled = false;
+        GetComponent<Collider2D>().enabled = false;
+        StartCoroutine(Appear());
+    }
+
+    private IEnumerator Appear()
+    {
+        yield return new WaitForSeconds(1);
+        GetComponent<SpriteRenderer>().enabled = true;
+        GetComponent<Collider2D>().enabled = true;
     }
 }
